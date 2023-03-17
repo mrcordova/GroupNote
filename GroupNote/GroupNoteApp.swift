094fda4bf100a909948e411008902ba8b0013df3
mrcordova <46950394+mrcordova@users.noreply.github.com>
2023-03-17 14:59:40 -0700

    Initial Commit

---
+++ GroupNote/GroupNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupNoteApp.swift
+//  GroupNote
+//
+//  Created by Noah Cordova on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
